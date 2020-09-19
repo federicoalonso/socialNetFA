016d68b757d90a4b6b0dfb24e64f116efc34abcd
federicoalonso <46576525+federicoalonso@users.noreply.github.com>
2020-09-18 22:05:21 -0300

    Feature: Generando login y registro
    Se empieza a generar la página de login y registro, queda pendiente la funcionalidad de la misma

--- socialnetfa/src/App.js
+++ socialnetfa/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useState } from 'react';
+import SignInSignUp from "./pages/SignInSignUp"
 
-function App() {
+export default function App() {
+
+  const [user, setUser] = useState(null);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (
+      <h1>Estas logueado</h1>
+      ) : (
+      <div>
+        <SignInSignUp />
+      </div>
+      )}
     </div>
-  );
+  )
 }
-
-export default App;
